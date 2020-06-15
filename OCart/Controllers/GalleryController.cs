b7749c988a0c5e6df795b4e31b4cd297fc57ab31
Polina <tristiamika778@gmail.com>
2020-06-15 23:24:58 +0700

    Create gallery controller and views

---
+++ OCart/Controllers/GalleryController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OCart.Data;
+using OCart.Models;
+
+namespace OCart.Controllers
+{
+    public class GalleryController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public GalleryController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: Gallery
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = context.Posts.Include(p => p.Category).Include(p => p.Creator);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Gallery/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await context.Posts
+                .Include(p => p.Category)
+                .Include(p => p.Creator)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // GET: Gallery/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(context.Categories, "Id", "Name");
+            ViewData["CreatorId"] = new SelectList(context.Set<ApplicationUser>(), "Id", "Id");
+            return View();
+        }
+
+        // POST: Gallery/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CreatorId,CategoryId,Created,Modified,Text")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                post.Id = Guid.NewGuid();
+                context.Add(post);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(context.Categories, "Id", "Name", post.CategoryId);
+            ViewData["CreatorId"] = new SelectList(context.Set<ApplicationUser>(), "Id", "Id", post.CreatorId);
+            return View(post);
+        }
+
+        // GET: Gallery/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await context.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(context.Categories, "Id", "Name", post.CategoryId);
+            ViewData["CreatorId"] = new SelectList(context.Set<ApplicationUser>(), "Id", "Id", post.CreatorId);
+            return View(post);
+        }
+
+        // POST: Gallery/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,CreatorId,CategoryId,Created,Modified,Text")] Post post)
+        {
+            if (id != post.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    context.Update(post);
+                    await context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostExists(post.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(context.Categories, "Id", "Name", post.CategoryId);
+            ViewData["CreatorId"] = new SelectList(context.Set<ApplicationUser>(), "Id", "Id", post.CreatorId);
+            return View(post);
+        }
+
+        // GET: Gallery/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await context.Posts
+                .Include(p => p.Category)
+                .Include(p => p.Creator)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // POST: Gallery/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var post = await context.Posts.FindAsync(id);
+            context.Posts.Remove(post);
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostExists(Guid id)
+        {
+            return context.Posts.Any(e => e.Id == id);
+        }
+    }
+}
