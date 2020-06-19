bd1dea5f13066643257c14e1e748e87dcb2ac0be
Polina <tristiamika778@gmail.com>
2020-06-19 21:35:55 +0700

    Add viewmodel for create and edit auction

---
+++ OCart/Models/ViewModels/AuctionCreateModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace OCart.Models.ViewModels
+{
+    public class AuctionCreateModel
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        public decimal InitialBet { get; set; }
+        [Required]
+        public Guid CategoryId { get; set; }
+    }
+}
