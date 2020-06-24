45e9b583cb146e5071847b5735a19c4d09f13c95
Polina <tristiamika778@gmail.com>
2020-06-24 19:49:40 +0700

    Add bets controller

---
+++ OCart/Controllers/BetsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OCart.Data;
+using OCart.Models;
+
+namespace OCart.Controllers
+{
+    public class BetsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BetsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Bets
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Bets.Include(b => b.Creator);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Bets/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bet = await _context.Bets
+                .Include(b => b.Creator)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bet == null)
+            {
+                return NotFound();
+            }
+
+            return View(bet);
+        }
+
+        // GET: Bets/Create
+        public IActionResult Create()
+        {
+            ViewData["CreatorId"] = new SelectList(_context.Set<ApplicationUser>(), "Id", "Id");
+            return View();
+        }
+
+        // POST: Bets/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,AuctionId,CreatorId,Cost")] Bet bet)
+        {
+            if (ModelState.IsValid)
+            {
+                bet.Id = Guid.NewGuid();
+                _context.Add(bet);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatorId"] = new SelectList(_context.Set<ApplicationUser>(), "Id", "Id", bet.CreatorId);
+            return View(bet);
+        }
+
+        // GET: Bets/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bet = await _context.Bets.FindAsync(id);
+            if (bet == null)
+            {
+                return NotFound();
+            }
+            ViewData["CreatorId"] = new SelectList(_context.Set<ApplicationUser>(), "Id", "Id", bet.CreatorId);
+            return View(bet);
+        }
+
+        // POST: Bets/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,AuctionId,CreatorId,Cost")] Bet bet)
+        {
+            if (id != bet.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bet);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BetExists(bet.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatorId"] = new SelectList(_context.Set<ApplicationUser>(), "Id", "Id", bet.CreatorId);
+            return View(bet);
+        }
+
+        // GET: Bets/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bet = await _context.Bets
+                .Include(b => b.Creator)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bet == null)
+            {
+                return NotFound();
+            }
+
+            return View(bet);
+        }
+
+        // POST: Bets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var bet = await _context.Bets.FindAsync(id);
+            _context.Bets.Remove(bet);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BetExists(Guid id)
+        {
+            return _context.Bets.Any(e => e.Id == id);
+        }
+    }
+}
