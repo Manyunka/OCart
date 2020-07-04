3b727b8591304f46ea5103189838db4c0a4868e4
Polina <tristiamika778@gmail.com>
2020-07-04 19:05:01 +0700

    Add commissions controller

---
+++ OCart/Controllers/CommissionsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OCart.Data;
+using OCart.Models;
+
+namespace OCart.Controllers
+{
+    public class CommissionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommissionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Commissions
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Commissions.Include(c => c.Category).Include(c => c.Creator);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Commissions/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var commission = await _context.Commissions
+                .Include(c => c.Category)
+                .Include(c => c.Creator)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (commission == null)
+            {
+                return NotFound();
+            }
+
+            return View(commission);
+        }
+
+        // GET: Commissions/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
+            ViewData["CreatorId"] = new SelectList(_context.Set<ApplicationUser>(), "Id", "Id");
+            return View();
+        }
+
+        // POST: Commissions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CreatorId,CategoryId,Created,Modified,Title,Description,Price")] Commission commission)
+        {
+            if (ModelState.IsValid)
+            {
+                commission.Id = Guid.NewGuid();
+                _context.Add(commission);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", commission.CategoryId);
+            ViewData["CreatorId"] = new SelectList(_context.Set<ApplicationUser>(), "Id", "Id", commission.CreatorId);
+            return View(commission);
+        }
+
+        // GET: Commissions/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var commission = await _context.Commissions.FindAsync(id);
+            if (commission == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", commission.CategoryId);
+            ViewData["CreatorId"] = new SelectList(_context.Set<ApplicationUser>(), "Id", "Id", commission.CreatorId);
+            return View(commission);
+        }
+
+        // POST: Commissions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,CreatorId,CategoryId,Created,Modified,Title,Description,Price")] Commission commission)
+        {
+            if (id != commission.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(commission);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CommissionExists(commission.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", commission.CategoryId);
+            ViewData["CreatorId"] = new SelectList(_context.Set<ApplicationUser>(), "Id", "Id", commission.CreatorId);
+            return View(commission);
+        }
+
+        // GET: Commissions/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var commission = await _context.Commissions
+                .Include(c => c.Category)
+                .Include(c => c.Creator)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (commission == null)
+            {
+                return NotFound();
+            }
+
+            return View(commission);
+        }
+
+        // POST: Commissions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var commission = await _context.Commissions.FindAsync(id);
+            _context.Commissions.Remove(commission);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CommissionExists(Guid id)
+        {
+            return _context.Commissions.Any(e => e.Id == id);
+        }
+    }
+}
